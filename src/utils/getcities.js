099ef634e3de1bdf8bf94f5e44e56e959634bed8
Muhammad Sohaib Ali <garenasohaib123@gmail.com>
2025-03-15 17:29:49 +0500

    Updated Weather App

--- src/utils/getcities.js
+++ src/utils/getcities.js
@@ -2,8 +2,7 @@ async function getRandomCities(countryName) {
     const countryCode = await getCountryCode(countryName === 'USA United States of America' ? 'United States' : countryName);
     if (!countryCode) return { error: "Invalid country name" };
     try {
-        // const response = await fetch(`https://secure.geonames.org/searchJSON?country=${countryCode}&featureClass=P&orderby=population&maxRows=50&username=${process.env.REACT_APP_USER_NAME}`);
-        const data = await (await fetch(`https://secure.geonames.org/searchJSON?country=${countryCode}&featureClass=P&orderby=population&maxRows=50&username=${process.env.REACT_APP_USER_NAME}`)).json();
+        const data = await (await fetch(`${process.env.REACT_APP_CITIES}=${countryCode}&${process.env.REACT_APP_CONF}=${process.env.REACT_APP_USER_NAME}`)).json();
         const cities = data.geonames?.filter(c => ["PPL", "PPLA", "PPLC"].includes(c.fcode)).map(c => c.name) || [];
         if (cities.length < 2) return { error: "Not enough cities found" };
         return [...Array(2)].map(() => cities.splice(Math.floor(Math.random() * cities.length), 1)[0]);
@@ -14,7 +13,7 @@ async function getRandomCities(countryName) {
 
 async function getCountryCode(countryName) {
     try {
-        return (await (await fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)).json())[0]?.cca2 || null;
+        return (await (await fetch(`${process.env.REACT_APP_CODE}/${countryName}?fullText=true`)).json())[0]?.cca2 || null;
     } catch {
         return null;
     }
